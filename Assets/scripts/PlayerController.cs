86c2b9714bb25d759b36e7768667f7e11c2d1aac
yahyah016 <TRAMIYAHe@nycstudents.net>
2024-11-15 09:23:45 -0500

    part5complete

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+       MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + -0.001f;
-        position.y = position.y + -0.001f;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
         transform.position = position;
     }
 }
