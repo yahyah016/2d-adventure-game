e152708e47c21ad6ff511f1cb6433dfe05186c43
yahyah016 <TRAMIYAHe@nycstudents.net>
2024-11-15 09:04:24 -0500

    Initial commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position;
+        position.x = position.x + -0.001f;
+        position.y = position.y + -0.001f;
+        transform.position = position;
+    }
+}
